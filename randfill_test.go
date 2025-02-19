f02418e650b2543356f8f2e4b13dc7b01bc87d1c
Tim Hockin <thockin@google.com>
2025-02-19 15:47:45 -0800

    Add kube copyright

--- randfill_test.go
+++ randfill_test.go
@@ -1,5 +1,7 @@
 /*
 Copyright 2014 Google Inc. All rights reserved.
+Copyright 2014 The gofuzz Authors.
+Copyright 2025 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
